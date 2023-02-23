3f15b1bd97df5b585f4ca0e63f11f11fee09edc1
Mushahira Muneer <68425509+mush192@users.noreply.github.com>
2023-02-23 21:29:12 +0530

    Add files via upload

---
+++ projectplant.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+from tensorflow.keras import models,layers
+import matplotlib.pyplot as plt
+
+BATCH_SIZE = 32
+IMAGE_SIZE = 256
+CHANNELS=3
+EPOCHS=50
+
+dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    "PlantVillage",
+    shuffle=True,
+    image_size=(IMAGE_SIZE,IMAGE_SIZE),
+    batch_size=BATCH_SIZE
+)
+
+class_names = dataset.class_names
+
+
+for image_batch, labels_batch in dataset.take(1):
+    print(image_batch.shape)
+    print(labels_batch.numpy())
+
+plt.figure(figsize=(16, 16))
+for image_batch, labels_batch in dataset.take(1):
+    for i in range(12):
+        ax = plt.subplot(3, 4, i + 1)
+        plt.imshow(image_batch[i].numpy().astype("uint8"))
+        plt.title(class_names[labels_batch[i]]) 
+        plt.axis("off")
+
+def get_dataset_partitions_tf(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
+    assert (train_split + test_split + val_split) == 1
+    
+    ds_size = len(ds)
+    
+    if shuffle:
+        ds = ds.shuffle(shuffle_size, seed=12)
+    
+    train_size = int(train_split * ds_size)
+    val_size = int(val_split * ds_size)
+    
+    train_ds = ds.take(train_size)    
+    val_ds = ds.skip(train_size).take(val_size)
+    test_ds = ds.skip(train_size).skip(val_size)
+    
+    return train_ds, val_ds, test_ds
+
+train_ds, val_ds, test_ds = get_dataset_partitions_tf(dataset)
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+val_ds = val_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+test_ds = test_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+
+resize_and_rescale = tf.keras.Sequential([
+  layers.experimental.preprocessing.Resizing(IMAGE_SIZE, IMAGE_SIZE),
+  layers.experimental.preprocessing.Rescaling(1./255),
+])
+
+data_augmentation = tf.keras.Sequential([
+  layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+  layers.experimental.preprocessing.RandomRotation(0.2),
+])
+
+
+
+
+
+
+
+
+      
